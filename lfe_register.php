6ff6229ce38288b4694840b689d3123162b4bf14
thaiveedutrust2018 <98946893+thaiveedutrust2018@users.noreply.github.com>
2022-04-09 18:08:07 +0530

    Update lfe_register.php

--- lfe_register.php
+++ lfe_register.php
@@ -1,6 +1,6 @@
 
 <?php
-include('webpage.php');
+
 $link= mysqli_connect("https://databases.000webhost.com/?_ga=2.228331229.1909350981.1649506330-363896670.1642482217", "id18728171_root", "july@2018_Asha","id18728171_thaiveedutrust"); // Establishing Connection with Server
 
 if(isset($_POST['submit']))
